30b57b88718ed012169b67b607e336d3a2dcf53c
nehakhadode <86253964+nehakhadode@users.noreply.github.com>
2022-05-02 07:23:35 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Ado.dotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;          
+            SqlConnection con = new SqlConnection(constring);
+            con.Open();
+            Console.WriteLine("Connection Successful.");
+            Console.ReadKey();
+        }
+    }
+}
